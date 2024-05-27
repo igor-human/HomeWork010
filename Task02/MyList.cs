a45d7fba98047767323ce8ed759960886516a684
Igor Korzhan <igor.human@gmail.com>
2024-05-27 22:24:37 +0300

    Добавьте файлы проекта.

---
+++ Task02/MyList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    public class MyList<T>
+    {
+        private T[] _items;
+        private int _count;
+
+        public MyList()
+        {
+            _items = new T[4];
+            _count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if(_count == _items.Length)
+            {
+                Array.Resize(ref _items, _items.Length + 5);
+            }
+            _items[_count] = item;
+            _count++;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _count)
+                {
+                    throw new IndexOutOfRangeException("Індекс виходить за межі");
+                }
+                return _items[index];
+            }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+        }
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Task02;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            Console.WriteLine("Елементи в MyList:");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            Console.WriteLine($"Всього номерів у масиві: {list.Count}");
+        }
+    }
+}
